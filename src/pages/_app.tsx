b1ec3e423cacb323624af038e30ac0a27fdc7840
Maria Helena Norén <marialinder97@hotmail.com>
2021-11-26 21:21:32 +0100

    rm static rendering

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -27,10 +27,3 @@ export default function MyApp({ Component, pageProps }: AppProps) {
     </CityProvider>
   );
 }
-
-MyApp.getInitialProps = async (appContext: AppContext) => {
-  // calls page's `getInitialProps` and fills `appProps.pageProps`
-  const appProps = await App.getInitialProps(appContext);
-
-  return { ...appProps };
-};
